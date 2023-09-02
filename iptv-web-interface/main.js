ed6a5438942c825b723b202540e84090b2b3d73f
i3cpu <jahongirtuychiyev677@gmail.com>
2023-09-03 01:43:53 +0500

    add iptv web files

---
+++ iptv-web-interface/main.js
@@ -0,0 +1,43 @@
+var app = new Vue({
+  el: '.main',
+  data: {
+    showChannelsBlock: true,
+    showPlayerBlock: false,
+    tg: window.Telegram.WebApp,
+    channel_name: '',
+  },
+  methods: {
+
+    onImageClick(channel_name) {
+      this.showPlayerBlock = true;
+      this.showChannelsBlock = false;
+      this.channel_name = channel_name;
+      this.$nextTick(function () {
+        this.autoplay(channel_name);
+      });
+    },
+
+    autoplay(channel_name) {
+      if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource('http://live.trkmillet.ru/millet/index.m3u8');
+        hls.attachMedia(this.$refs.video);
+        this.$refs.video.play();
+      } else if (this.$refs.video.canPlayType('application/vnd.apple.mpegurl')) {
+        this.$refs.video.src = 'http://live.trkmillet.ru/millet/index.m3u8';
+        this.$refs.video.addEventListener('loadedmetadata', function () {
+          this.$refs.video.play();
+        }.bind(this));
+      }
+    }, 
+
+    close_webapp() {
+      var context = {
+        status: "success"
+      }
+
+      this.tg.sendData(JSON.stringify(context));
+      this.tg.close();
+    }
+  },
+});
