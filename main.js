46daaf69f01a03694fa6496588a2a48755dab1b4
Джахонгир <106595656+i3cpu@users.noreply.github.com>
2023-09-20 03:39:12 +0500

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ var app = new Vue({
     showPlayerBlock: false,
     tg: window.Telegram.WebApp,
     channel_name: '',
+    videoSources: {
+      zortv: 'https://s04.watcher.uz/6/video.m3u8?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250cmFjdF9pZCI6MjcwMTc3LCJsb2dpbiI6InR2LTI3MDE3NyIsInRlbXBvcmFyeSI6ZmFsc2UsImlzX29hdXRoIjpmYWxzZSwiY3JlYXRlZF90aW1lIjoxNjUwMjgzMDI3fQ.LadLj4b6plIxTGCWntQQqsQGZ3R9EsvwhaXQd1sur2g%3A15',
+      milliy: 'https://s05.watcher.uz/70/video.m3u8?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250cmFjdF9pZCI6MjcwMTc3LCJsb2dpbiI6InR2LTI3MDE3NyIsInRlbXBvcmFyeSI6ZmFsc2UsImlzX29hdXRoIjpmYWxzZSwiY3JlYXRlZF90aW1lIjoxNjUwMjgzMDI3fQ.LadLj4b6plIxTGCWntQQqsQGZ3R9EsvwhaXQd1sur2g%3A15',
+      uzreport: 'https://s03.watcher.uz/75/video.m3u8?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250cmFjdF9pZCI6MjcwMTc3LCJsb2dpbiI6InR2LTI3MDE3NyIsInRlbXBvcmFyeSI6ZmFsc2UsImlzX29hdXRoIjpmYWxzZSwiY3JlYXRlZF90aW1lIjoxNjUwMjgzMDI3fQ.LadLj4b6plIxTGCWntQQqsQGZ3R9EsvwhaXQd1sur2g%3A15',
+      sport: 'https://s04.watcher.uz/59/video.m3u8?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250cmFjdF9pZCI6MjcwMTc3LCJsb2dpbiI6InR2LTI3MDE3NyIsInRlbXBvcmFyeSI6ZmFsc2UsImlzX29hdXRoIjpmYWxzZSwiY3JlYXRlZF90aW1lIjoxNjUwMjgzMDI3fQ.LadLj4b6plIxTGCWntQQqsQGZ3R9EsvwhaXQd1sur2g%3A15'
+    }
   },
   methods: {
 
@@ -18,17 +24,27 @@ var app = new Vue({
     },
 
     autoplay(channel_name) {
-      if (Hls.isSupported()) {
-        var hls = new Hls();
-        hls.loadSource('https://live.trkmillet.ru/millet/index.m3u8');
-        hls.attachMedia(this.$refs.video);
-        this.$refs.video.play();
-      } else if (this.$refs.video.canPlayType('application/vnd.apple.mpegurl')) {
-        this.$refs.video.src = 'https://live.trkmillet.ru/millet/index.m3u8';
-        this.$refs.video.addEventListener('loadedmetadata', function () {
-          this.$refs.video.play();
-        }.bind(this));
+    
+      if (channel_name){
+        streamUrl = this.videoSources[channel_name]
       }
+
+      if (streamUrl){
+
+        if (Hls.isSupported()) {
+          var hls = new Hls();
+          hls.loadSource(streamUrl);
+          hls.attachMedia(this.$refs.video);
+          this.$refs.video.play();
+        } else if (this.$refs.video.canPlayType('application/vnd.apple.mpegurl')) {
+          this.$refs.video.src = streamUrl;
+          this.$refs.video.addEventListener('loadedmetadata', function () {
+            this.$refs.video.play();
+          }.bind(this));
+        }
+      }
+
+
     }, 
 
     close_webapp() {
