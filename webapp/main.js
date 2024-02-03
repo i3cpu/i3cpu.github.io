c156c9909989f90ce46984d501d1107360b7fe22
Джахонгир <106595656+i3cpu@users.noreply.github.com>
2024-02-03 19:15:03 +0500

    Add files via upload

---
+++ webapp/main.js
@@ -0,0 +1,44 @@
+var app = new Vue({
+    el: '.main',
+    data: {
+  
+      showInfoBlock: true,
+      showFormBlock: false,
+      first_name:'',
+      adress:'',
+      phone:'',
+      date:'',
+      comments:'',
+      tg:window.Telegram.WebApp,
+      
+    },
+    mounted:function(){
+        if (this.tg.isExpanded == false){
+          this.tg.expand()
+        }
+      },
+    methods: {
+      to_form(){
+        this.showFormBlock = true;
+        this.showInfoBlock = false
+      },
+      send_data(){
+  
+        context = {
+            first_name_name: this.city_name,
+            adress: this.adress,
+            phone:this.phone,
+            date:this.date,
+            comments:this.comments,
+            status:"succes"
+        }
+  
+        this.tg.sendData(JSON.stringify(context));
+        this.tg.close();
+      }
+    },
+    close(){
+        this.tg.close();
+      },
+    
+  });
